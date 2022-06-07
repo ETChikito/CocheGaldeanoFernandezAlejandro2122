16ebbeff21ffab1e6ad0eb05dd20b40443a70f0a
Alejandro <Alejandro@DESKTOP-3V9G2R8>
2022-06-07 22:34:27 +0200

    Modificación de la clase Main GaldeanoFernandezAlejandro

--- src/coche/Main.java
+++ src/coche/Main.java
@@ -11,27 +11,38 @@ public class Main {
         Coche miCoche;
         int stockActual;
         
-        miCoche = new Coche("Opel",12000,500);
-        try 
-        {
-            System.out.println("Venta de Coches");
-            miCoche.vender(300);
-        } catch (Exception e)
-        {
-            System.out.print("Fallo al vender");
-        }
+        miCoche = operatoriaVentaGaldeanoFernándezAlejandro2122();
         
-        try
-        {
-            System.out.println("Compra de Coches");
-            miCoche.comprar(500);
-        } catch (Exception e)
-        {
-            System.out.print("Fallo al ingresar");
-        }
-        stockActual = miCoche.obtenerStock();
-        System.out.println("El stock actual es"+ stockActual );
+        operatoriaCompraGaldeanoFernándezAlejandro2122(miCoche);
     }
+
+	private static void operatoriaCompraGaldeanoFernándezAlejandro2122(Coche miCoche) {
+		int stockActual;
+		try
+		{
+			System.out.println("Compra de Coches");
+			miCoche.comprar(500);
+		} catch (Exception e)
+		{
+			System.out.print("Fallo al ingresar");
+		}
+		stockActual = miCoche.obtenerStock();
+		System.out.println("El stock actual es"+ stockActual );
+	}
+
+	private static Coche operatoriaVentaGaldeanoFernándezAlejandro2122() {
+		Coche miCoche;
+		miCoche = new Coche("Opel",12000,500);
+		try
+		{
+			System.out.println("Venta de Coches");
+			miCoche.vender(300);
+		} catch (Exception e)
+		{
+			System.out.print("Fallo al vender");
+		}
+		return miCoche;
+	}
 
 }
     
